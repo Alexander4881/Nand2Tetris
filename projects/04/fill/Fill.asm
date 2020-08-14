164c7276288440ef4922d2efc399a8b43a6e345f
Alexander Lihn Mikkelsen <alex6223@zbc.dk>
2020-08-14 10:29:06 +0200

    project 3 and 4 done

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,102 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Rectangle.asm
+// Draw a filled rectangle at the screen's top left corner.
+// The rectangle's width is 16 pixels, and its height is RAM[0].
+// Usage: put a non-negative number (rectangle's height) in RAM[0]
+
+// for (i=0; i < n; i++) P
+//		draw 16 black pixels at the beginning of row i
+// }
+
+//  addr = SCREEN
+//  n = RAM[0]
+//  i = 0
+//
+//  LOOP:
+//  	if i > n goto END
+//  	  RAM[addr] = -1 	// 111111111111111
+//  	// advance to next row
+//  	addr = addr + 32	// 32 words ina row (32 * 16 = 512)
+//  	i = i + 1
+//  	goto LOOP
+//  END:
+//  	goto END
+
+
+// set up a var n with the value of 10
+(RESTART)
+@24575
+//@16386
+D=A
+@screenMaxNum
+M=D
+
+@color
+M=-1
+
+// set up i with a value of 1
+@16384
+D=A
+@i
+M=D
+
+// insert if
+// if d > 0
+@24576
+D=M
+@IF
+D;JEQ
+
+@ELSE
+D=A
+D;JMP
+
+// if
+(IF)
+@color
+M=0
+@LOOP
+D=A
+D;JMP
+
+
+// else
+(ELSE)
+@color
+M=-1
+@LOOP
+D=A
+D;JMP
+
+@color
+M=-1
+
+
+(LOOP)
+   @i
+   D = M
+   @screenMaxNum
+   D = D - M
+   @RESTART
+   D;JGT		// if i > n goto END
+
+   // // add two numbers
+   @i
+   D=M
+   
+   @color
+   D=M
+   
+   @i
+   A=M
+   M=D
+
+   @i
+   M = M + 1	// i = i + 1
+   
+   // go to 
+   @LOOP
+   0;JEQ		// goto LOOP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,53 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+
+// set up a var n with the value of 10
+@10
+D=A
+@firstNumber
+M=D
+
+// set up a var n with the value of 10
+@10
+D=A
+@secondNumber
+M=D
+
+// set up i with a value of 1
+@2
+D=A
+@i
+M=D
+
+// // add two numbers
+// @n
+// D=M
+// M = D+M
+
+
+(LOOP)
+   @i
+   D = M
+   @firstNumber
+   D = D - M
+   @END
+   D;JGT		// if i > n goto END
+
+   // // add two numbers
+   @firstNumber
+   D=M
+   @secondNumber
+   M = D+M
+   
+   @i
+   M = M + 1	// i = i + 1
+   
+   // go to 
+   @LOOP
+   0;JMP		// goto LOOP
+
+  (END)
+     @END		// program's end
+     0;JMP		// infinite loop
